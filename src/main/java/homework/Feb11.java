af0d67f58b43c0aa5fb57edbca99ee9edc36aebb
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-02-12 07:49:16 +0700

    homework for feb 11

--- src/main/java/homework/Feb11.java
+++ src/main/java/homework/Feb11.java
@@ -1,5 +1,24 @@
 package homework;
 
 public class Feb11 {
+    public static void main(String[] args) {
+        var str = "Hello World";
+        var result = "";
+//        result = countVowels(str);
+//        result = countWords(str);
+//        printDigits(str);
+        System.out.println(result);
+    }
 
+    // 1. Write a method that takes a string and returns the number of vowels (a, e, i, o, u) in the string.
+    // public static int countVowels(String str)
+
+    // 2. Write a method that takes a string and returns the number of words in the string.
+    // Example: "Hello World" -> 2
+    // public static int countWords(String str)
+
+    // 3. Write a method that takes a string and print the digits in the string.
+    // Hint: Character.isDigit()
+    // Example: "Hello 123 Worl45d" -> "12345"
+    // public static void printDigits(String str)
 }
