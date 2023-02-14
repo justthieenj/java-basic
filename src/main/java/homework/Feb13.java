822539b12d4cf1621dde0ed9ed3996a6436ca63e
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-02-14 08:52:52 +0700

    fix Feb13

--- src/main/java/homework/Feb13.java
+++ src/main/java/homework/Feb13.java
@@ -6,12 +6,12 @@ public class Feb13 {
     }
 
     // round a double to 2 decimal places using floor and ceil
-//    public static double roundNumber(double num)
+    // public static double roundNumber(double num)
 
     // random an int between 1 and 99
-//    public static double randomNumber(double num)
+    // public static double randomNumber()
 
     // find and count a specific string in a list of strings using forEach
-//    public static int countString(List<String> list, String str)
+    // public static int countString(List<String> list, String str)
     // Example: countString(List.of("abc", "def", "abc"), "abc") -> 2
 }
