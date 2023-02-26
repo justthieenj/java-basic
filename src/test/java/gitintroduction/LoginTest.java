0bb67d9862df8ded6b4cbfefa27771cbcf28e21d
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-02-26 10:03:45 +0700

    update line 9 as comment

--- src/test/java/gitintroduction/LoginTest.java
+++ src/test/java/gitintroduction/LoginTest.java
@@ -6,7 +6,7 @@ public class LoginTest {
         System.out.println("Test 2  - Login with invalid credentials");
         System.out.println("Test 3  - Login with invalid password");
         System.out.println("Test 4  - Login 4");
-        System.out.println("Test 5  - Login 5");
+        System.out.println("Test 5  - Login 5 updated");
         System.out.println("Test 6  - Login 6");
         System.out.println("Test 7  - Login 7");
     }
