dbfd2a41a42135076dc430a0e82ebaef17f39264
Thien Nguyen <40391726+justthieenj@users.noreply.github.com>
2023-02-26 10:08:25 +0700

    Add login test 4-7 (#1)
    * add test 4,5
    
    * add test 6 7
    
    * update line 9 as comment

--- src/test/java/gitintroduction/LoginTest.java
+++ src/test/java/gitintroduction/LoginTest.java
@@ -5,5 +5,9 @@ public class LoginTest {
         System.out.println("Test 1  - Login");
         System.out.println("Test 2  - Login with invalid credentials");
         System.out.println("Test 3  - Login with invalid password");
+        System.out.println("Test 4  - Login 4");
+        System.out.println("Test 5  - Login 5 updated");
+        System.out.println("Test 6  - Login 6");
+        System.out.println("Test 7  - Login 7");
     }
 }
