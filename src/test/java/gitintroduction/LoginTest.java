9bbb4139119486dff92c3e9435e4c7a8c2117018
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-02-26 08:54:24 +0700

    create login test

---
+++ src/test/java/gitintroduction/LoginTest.java
@@ -0,0 +1,9 @@
+package gitintroduction;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        System.out.println("Test 1  - Login");
+        System.out.println("Test 2  - Login with invalid credentials");
+        System.out.println("Test 3  - Login with invalid password");
+    }
+}
