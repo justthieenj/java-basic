e026d5986e48c674937d0c5f0b7cc6674ade3fb1
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-02-27 21:19:41 +0700

    update homework Feb27

--- src/main/java/homework/Feb27.java
+++ src/main/java/homework/Feb27.java
@@ -1,7 +1,10 @@
 package homework;
 
+import java.util.List;
+
 public class Feb27 {
     public static void main(String[] args) {
-        // Read the text-file.txt (located in src/test/resources/) and print out the content of the file.
+        // 1. Read the text-file.txt (located in src/test/resources/) and print out the content of the file.
+        // 2. Read the fruits.txt (located in src/test/resources/) and stored the content of the file in a List<String>.
     }
 }
