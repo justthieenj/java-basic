126c363ae9f568a485135df10274b233d3698090
Nguyen Cao Hoang Lan <nchoanglan94@gmail.com>
2023-02-27 14:20:36 +0700

    Done Feb26 homework

--- src/main/java/homework/Feb26.java
+++ src/main/java/homework/Feb26.java
@@ -9,15 +9,37 @@ public class Feb26 {
     2. fix the inputArray() method to accept any number of elements
     */
     public static void main(String[] args) {
+
+        // Handle the exception and print "Please input 5 elements" if the number of elements is not 5
+        try {
+            System.out.println("Input elements of array:");
+            int[] result = inputArray(54, 2, 78, 1, 5); // valid
+            int[] result2 = inputArray(4, 25, 74, 1, -5, 6, 2, 17); // error
+            System.out.println("The result array: " + Arrays.toString(result2));
+            System.out.println("The result sorted ascending array: " + sortAscending(result2));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please input 5 elements");
+        }
+
+        // Using fixed inputArray() method to accept any number of elements
+        System.out.println();
         System.out.println("Input elements of array:");
-        int[] result = inputArray(54, 2, 78, 1, 5); // valid
-        int[] result2 = inputArray(4, 25, 74, 1, -5, 6, 2, 17); // error
-        System.out.println("The result array: " + Arrays.toString(result));
-        System.out.println("The result sorted ascending array: " + sortAscending(result));
+        int[] result3 = inputArrayNew(7, 90, 23, 47, 22, 95, 34, 74, 23);
+        System.out.println("The result array: " + Arrays.toString(result3));
+        System.out.println("The result sorted ascending array: " + sortAscending(result3));
     }
 
     private static int[] inputArray(int... elements) {
         int[] array = new int[5];
+        for (int i = 0; i < elements.length; i++) {
+            array[i] = elements[i];
+        }
+        return array;
+    }
+
+    // New Input Array method that accept any number of elements
+    private static int[] inputArrayNew(int... elements) {
+        int[] array = new int[elements.length];
         for (int i = 0; i < elements.length; i++) {
             array[i] = elements[i];
         }
