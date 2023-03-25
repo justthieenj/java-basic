35c6a1670d46bb450e7cb9a44563f4d6beada6cc
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-03-25 22:26:29 +0700

    regex

--- src/test/java/regex/RegularExpressionTest.java
+++ src/test/java/regex/RegularExpressionTest.java
@@ -7,6 +7,11 @@ public class RegularExpressionTest {
             WebDriver drives a browser natively, as a user would, either locally or on a remote machine using the Selenium server, marks a leap forward in terms of browser automation.
             Selenium WebDriver refers to both the language bindings and the implementations of the individual browser controlling code. This is commonly referred to as just WebDriver.
             """;
+
+    private static final String TEXT = """
+            The cat fat ran down the street.
+            It was searching for a mouse to eat.
+            """;
     private static final List<String> biensoxe = List.of("43A-12345", "43A-123456", "59A-1234567", "59A-12345678", "29D-123456789", "43D-1234567890");
     // yyyy/MM/dd
     private static final List<String> dob = List.of("1990/02/09", "2000/99/09");
